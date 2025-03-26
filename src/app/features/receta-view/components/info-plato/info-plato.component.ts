19aef1621290ebf9af1479891a45ef0cb3bc5e07
davidgm26 <davidgama260402@gmail.com>
2025-03-26 21:36:05 +0100

    Cambios en plantilla de informacion

--- src/app/features/receta-view/components/info-plato/info-plato.component.ts
+++ src/app/features/receta-view/components/info-plato/info-plato.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { IonIcon } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-info-plato',
-  imports: [],
+  imports: [IonIcon],
   templateUrl: './info-plato.component.html',
   styleUrl: './info-plato.component.css'
 })
