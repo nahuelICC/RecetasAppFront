5b851774f343522e24c34804ab0dbc0a1af7f9c8
Luis <lmarincarmona@safareyes.es>
2025-04-27 20:10:10 +0200

    componente crearreceta

---
+++ src/app/features/crear-receta/crear-receta.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+
+import { CrearRecetaComponent } from './crear-receta.component';
+
+describe('CrearRecetaComponent', () => {
+  let component: CrearRecetaComponent;
+  let fixture: ComponentFixture<CrearRecetaComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [CrearRecetaComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CrearRecetaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/features/crear-receta/crear-receta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-receta',
+  templateUrl: './crear-receta.component.html',
+  styleUrls: ['./crear-receta.component.css'],
+  standalone: true,
+})
+export class CrearRecetaComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
