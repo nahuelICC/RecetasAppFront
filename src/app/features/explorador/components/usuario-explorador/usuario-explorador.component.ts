edec20d55bfb1433dc768fd5cac9f6275014fb8a
Lucia <lmerinodelgado@safareyes.es>
2025-05-19 16:25:37 +0200

    Boton seguir del buscador conectado con la función

--- src/app/features/explorador/components/usuario-explorador/usuario-explorador.component.ts
+++ src/app/features/explorador/components/usuario-explorador/usuario-explorador.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, NgZone, OnInit} from '@angular/core';
 import {UsuarioExploradorDTO} from '../../models/UsuarioExploradorDTO';
 import {Router, RouterLink} from '@angular/router';
 import {EncryptService} from '../../../../core/services/encrypt.service';
+import {UsuarioService} from '../../../usuario/services/usuario.service';
 
 @Component({
   selector: 'app-usuario-explorador',
@@ -16,15 +17,23 @@ export class UsuarioExploradorComponent  implements OnInit {
   @Input() userData!: UsuarioExploradorDTO;
   @Input() seguido: boolean = false;
 
-  constructor(private router:Router,private zone: NgZone,private encryptService:EncryptService) { }
+  constructor(private router:Router,private zone: NgZone,private encryptService:EncryptService,private usuarioService: UsuarioService) { }
 
   ngOnInit() {}
   toggleFollow(event: MouseEvent): void {
     event.stopPropagation();
-    console.log('Like clicked for recipe:', this.userData?.id);
-    this.seguido = !this.seguido;
+    this.usuarioService.changeSeguir(this.userData.id.toString()).subscribe({
+      next: () => {
+        this.seguido = !this.seguido;
+        console.log(`${this.seguido ? 'Seguido' : 'Dejado de seguir'} al usuario:`, this.userData.id);
+      },
+      error: err => {
+        console.error('Error al intentar seguir/dejar de seguir:', err);
+      }
+    });
 
   }
+
 
   redireccionarPerfil(id: string): void {
     this.zone.run(() => {
