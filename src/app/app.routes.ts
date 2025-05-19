68ec8d75041d3b2fa2fe6ffd4f9f6e032ef940c4
David Garcia Maria <davidgama260402@gmail.com>
2025-05-20 00:19:49 +0200

    enrutado para rutas fuera de las estipuladas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -35,4 +35,5 @@ export const routes: Routes = [
     path: 'admin',
     loadChildren: () => import('./features/admin/admin.routes').then(m => m.ADMIN_ROUTES)
   },
+  {path: "**", redirectTo: 'main', pathMatch: 'full'},
 ];
