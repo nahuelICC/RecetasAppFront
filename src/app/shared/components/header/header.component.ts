5f0bb84ab61393a34a6f5c6206a4f19d10396659
rafdiecas <rdiezcastro@safareyes.es>
2025-05-22 21:22:06 +0200

    mod - inicio del modo oscuro

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -6,6 +6,7 @@ import {NgIf} from '@angular/common';
 import {AuthService} from '../../../core/services/auth.service';
 import {HeaderService} from '../../services/header.service';
 import {RouterLink} from '@angular/router';
+import {ioniconContent} from 'ionicons/dist/types/components/icon/request';
 
 @Component({
   selector: 'app-header',
@@ -24,6 +25,7 @@ export class HeaderComponent  implements OnInit {
   isMobile: boolean;
   menuOpen: boolean = false;
   imagenPerfil: string = 'https://ionicframework.com/docs/img/demos/avatar.svg';
+  isLightMode = false;
 
   constructor(private platform: Platform,public authService: AuthService, private headerService: HeaderService) {
     this.isMobile = this.platform.width() < 768;
@@ -39,6 +41,9 @@ export class HeaderComponent  implements OnInit {
         this.imagenPerfil = response;
       }
     });
+    const savedTheme = localStorage.getItem('theme');
+    this.isLightMode = savedTheme === 'light';
+    this.applyTheme();
   }
 
   /**
@@ -51,6 +56,24 @@ export class HeaderComponent  implements OnInit {
 
   toggleMenu(): void {
     this.menuOpen = !this.menuOpen;
+  }
+
+  toggleTheme() {
+    this.isLightMode = !this.isLightMode;
+    localStorage.setItem('theme', this.isLightMode ? 'light' : 'dark');
+    this.applyTheme();
+  }
+
+
+  applyTheme() {
+    const classList = document.documentElement.classList;
+    if (this.isLightMode) {
+      classList.add('light-theme');
+      classList.remove('dark-theme');
+    } else {
+      classList.remove('light-theme');
+      classList.add('dark-theme');
+    }
   }
 
 
